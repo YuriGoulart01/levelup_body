d6378fd7727ca1b9e160f78d876e7849ddab7a5e
rafaela <rafaelas@genstudents.org>
2026-02-04 09:27:59 -0300

    feat: login

--- levelup-body/src/App.tsx
+++ levelup-body/src/App.tsx
@@ -1,27 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import "./App.css";
+import { Cadastro } from "./pages/cadastro/Cadastro";
 
 export default function App() {
   return (
     <Routes>
+       <Route path="/" element={<Cadastro />} />
       <Route path="/login" element={<Login />} />
 
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Cadastro } from "./pages/Cadastro";
-import Login from "./pages/Login";
 
-
-export function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Cadastro />} />
-        <Route path="/login" element={<Login/>}/>
-
-      </Routes>
-    </BrowserRouter>
   );
 }
