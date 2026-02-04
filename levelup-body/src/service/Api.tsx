1d964cb29785889d65bb520896117934e88c8d4c
rafaela <rafaelas@genstudents.org>
2026-02-04 10:12:32 -0300

    fix:login

--- levelup-body/src/service/Api.tsx
+++ levelup-body/src/service/Api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://levelupbody-api-1-o76k.onrender.com",
 });
 
 export const login = async (usuario: string, senha: string) => {
--- levelup-body/src/service/api.ts
+++ levelup-body/src/service/api.ts
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000", 
+  baseURL: "https://levelupbody-api-1-o76k.onrender.com", 
 });
