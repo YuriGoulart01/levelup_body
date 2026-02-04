cecb7ab189e5b2a236d7ea849a29ffdcb0fd9563
Eduardo Duarte Cunha <edu.duartecunha@gmail.com>
2026-02-04 11:52:42 -0300

    feat: implementando Home e Login

--- levelup-body/src/App.tsx
+++ levelup-body/src/App.tsx
@@ -3,11 +3,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import { Cadastro } from "./pages/cadastro/Cadastro";
 import { PrivateRoute } from "./routes/PrivateRoute";
+import Home from "./pages/home/Home";
 
-// 👉 Exemplo de página protegida
-function Home() {
-  return <h1>Home protegida</h1>;
-}
 
 export default function App() {
   return (
@@ -18,13 +15,16 @@ export default function App() {
 
       {/* 🔐 Rotas privadas */}
       <Route
-        path="/"
+        path="/home"
         element={
           <PrivateRoute>
             <Home />
           </PrivateRoute>
         }
       />
+
+      {/* 🔁 Redirecionamento padrão */}
+      <Route path="/" element={<Navigate to="/home" replace />} />
 
       {/* 🚫 Fallback */}
       <Route path="*" element={<Navigate to="/login" replace />} />
